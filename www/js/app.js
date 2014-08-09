95c623ceb879bac825fd0abec83a344605ee2c63
ez2sugul <seunghoon100@gmail.com>
2014-08-09 13:01:14 +0900

    add archive event handler

--- www/js/app.js
+++ www/js/app.js
@@ -11,4 +11,24 @@ todoApp.controller('todoCtrl', function ($scope) {
 		$scope.todoList.push({done: false, title : newTitle});
 		$scope.newTitle = '';
 	}
+
+	$scope.archive = function () {
+		for (var i = $scope.todoList.length - 1; i >= 0; i--) {
+			if ($scope.todoList[i].done) {
+				$scope.todoList.splice(i, 1);
+			}
+		}
+	}
+
+	$scope.remain = function () {
+		var remainCount = 0;
+
+		angular.forEach($scope.todoList, function (value, key) {
+			if (value.done == false) {
+				remainCount++;
+			}
+		});
+
+		return remainCount;
+	}
 });
